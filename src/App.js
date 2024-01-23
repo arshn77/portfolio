f6b91297d6cd3590d9134c0e52a9e6fa192db1a3
Arshia <arshia@CCS-G40MPN3>
2024-01-23 04:41:32 -0500

    feat: finished basic portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Project from "./Project";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-5xl m-4 font-semibold">Arshia's Portfolio</h1>
+      <div className="flex items-center flex-col">
+        <Project
+          style="rounded-tl-lg rounded-tr-lg mt-4"
+          name="Testing Center"
+          link="https://arshn77.pythonanywhere.com/"
+          img="/images/testing-center.png"
+          code="https://github.com/arshn77/Testing-Hub"
+        />
+        <Project
+          name="Generative Blocks"
+          link="https://arshn77.github.io/generative-blocks/"
+          img="/images/gen_blocks.png"
+          code="https://github.com/arshn77/generative-blocks"
+        />
+        <Project
+          name="Asteroid Game"
+          link="https://arshn77.github.io/asteroid-game/"
+          img="/images/asteroid.png"
+          code=""
+        />
+        <Project
+          name="Itunes Artist Search"
+          link="https://arshn77.github.io/artist-search/"
+          img="/images/itunes-logo.png"
+          code=""
+          style="rounded-bl-lg rounded-br-lg mb-4"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Project.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Project({ name, link, img, style, code }) {
+  return (
+    <div
+      className={`w-1/3 text-lg border border-black flex items-center justify-between overflow-hidden ${style}`}
+    >
+      <img className="w-32 h-32 object-cover" src={img}></img>
+      <a
+        className="m-4 font-semibold hover:underline"
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {name}
+      </a>
+      {code ? (
+        <a
+          className="m-4 font-semibold hover:underline"
+          href={code}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Code
+        </a>
+      ) : (
+        <p className="m-4 font-semibold invisible">Code</p>
+      )}
+    </div>
+  );
+}
