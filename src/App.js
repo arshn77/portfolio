2b1fb8e49c24a5abb3a9ae28ee7b2f030395d069
Arshia <arshia@CCS-G40MPN3>
2024-01-25 20:09:25 -0500

    fix:  vercel analytics

--- src/App.js
+++ src/App.js
@@ -9,38 +9,40 @@ import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <div className="App">
-      <Analytics />
-      <h1 className="text-5xl m-4 font-semibold">Arshia's Portfolio</h1>
-      <div className="flex items-center flex-col">
-        <Project
-          style="rounded-tl-lg rounded-tr-lg mt-4"
-          name="Testing Center"
-          link="https://arshn77.pythonanywhere.com/"
-          img="/images/testing-center.png"
-          code="https://github.com/arshn77/Testing-Hub"
-        />
-        <Project
-          name="Generative Blocks"
-          link="https://arshn77.github.io/generative-blocks/"
-          img="/images/gen_blocks.png"
-          code="https://github.com/arshn77/generative-blocks"
-        />
-        <Project
-          name="Asteroid Game"
-          link="https://arshn77.github.io/asteroid-game/"
-          img="/images/asteroid.png"
-          code=""
-        />
-        <Project
-          name="Itunes Artist Search"
-          link="https://arshn77.github.io/artist-search/"
-          img="/images/itunes-logo.png"
-          code=""
-          style="rounded-bl-lg rounded-br-lg mb-4"
-        />
+    <>
+      <div className="App">
+        <h1 className="text-5xl m-4 font-semibold">Arshia's Portfolio</h1>
+        <div className="flex items-center flex-col">
+          <Project
+            style="rounded-tl-lg rounded-tr-lg mt-4"
+            name="Testing Center"
+            link="https://arshn77.pythonanywhere.com/"
+            img="/images/testing-center.png"
+            code="https://github.com/arshn77/Testing-Hub"
+          />
+          <Project
+            name="Generative Blocks"
+            link="https://arshn77.github.io/generative-blocks/"
+            img="/images/gen_blocks.png"
+            code="https://github.com/arshn77/generative-blocks"
+          />
+          <Project
+            name="Asteroid Game"
+            link="https://arshn77.github.io/asteroid-game/"
+            img="/images/asteroid.png"
+            code=""
+          />
+          <Project
+            name="Itunes Artist Search"
+            link="https://arshn77.github.io/artist-search/"
+            img="/images/itunes-logo.png"
+            code=""
+            style="rounded-bl-lg rounded-br-lg mb-4"
+          />
+        </div>
       </div>
-    </div>
+      <Analytics />
+    </>
   );
 }
 
